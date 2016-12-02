35065be3e57047a6105812237ce49cb64f43c215
Theo Fountain III <Theoiiifountain@gmail.com>
2016-12-02 17:59:53 -0500

    Update Qcam.py

--- Qcam.py
+++ Qcam.py
@@ -21,15 +21,43 @@ class Qcam(object):
         cam.QCam_OpenCamera(self.camId,byref(self.myHandle))
 
     def __del__(self):
-        cam.killQCam(self.myHandle)
+        cam.QCam_CloseCamera(self.myHandle)
         cam.QCam_ReleaseDriver()
         
     def setQCam(self):
-        cam.QCam_SetParam(byref(),)
+        settings = QCam_SettingsEx()
+       # imgForm = QCam_ImageFormat()
+        imgForm = qfmtMono16
+        cooler = 0 
+        cam.QCam_GetInfo(self.myHandle,qinfCcd, byref(ccdType))
+        cam.QCam_CreateCameraSettingsStruct(byref(settings))
+        cam.QCam_InitializeCameraSettings(self.myHandle,byref(settings))
+        cam.QCam_ReadDefaultSettings(self.myHandle,byref(settings))
+        cam.QCam_GetParamMin(byref(settings),qprmExposure,expMin)
+        cam.QCam_GetParamMax(byref(settings,qprmExposure,expMax)
+        cam.QCam_SetParam(byref(settings),qprmCoolerActive,cooler)
+        cam.QCam_SetParam(byref(settings),qprmExposure,exposure)
+        cam.QCam_SetParam(byref(settings),qprmImageFormat,imgForm)
+        if exposure < expMin:
+             exposure = expMin
+             
+        if exposure > expMax:
+             exposure = expMax
+             
+        cam.QCam_SendSettingstoCam(self.myHandle,byref(settings))
+    
+      
+         
         
-
-        return 0
+         
+        
 #
     def snapMono(self):
-       # cam.QCam_GrabFrame(self.myHandle, byref(Qcam_Frame))
-        cam.QCam_GrabFrame(self.myHandle)
+        frame = QCam_Frame()
+        
+        cam.QCam_Getinfo(self.myHandle,qinfImageSize,byref(imgSize))
+        cam.QCam_GrabFrame(self.myHandle,byref(frame))
+        
+        del frame.pbuffer
+        
+        
